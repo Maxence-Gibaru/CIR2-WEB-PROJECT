61f8b0e492ed320e74e212555655aa1e22d4f853
Maxence GIBARU <maxence.gibaru@student.junia.com>
2024-04-17 20:55:56 +0200

    pomodoro update

--- public/scripts/pomodoro.js
+++ public/scripts/pomodoro.js
@@ -1,24 +1,26 @@
+import { createButton } from "./dom-content/elementCreator.js";
+
 function pad(number) {
-  return number < 10 ? '0' + number : number;
+  return number < 10 ? "0" + number : number;
 }
 
-let timerDisplay = document.getElementById('timerDisplay');
+let timerDisplay = document.getElementById("timerDisplay");
 let isWorkSession = true;
-let workDuration = 25; 
+let workDuration = 25;
 
 let time = {
   minutes: workDuration,
   secondes: 0,
   timer: null,
-  displayTime: function() {
+  displayTime: function () {
     timerDisplay.textContent = `${pad(this.minutes)}:${pad(this.secondes)}`;
   },
-  setTime: function(minutesValue, secondesValue) {
+  setTime: function (minutesValue, secondesValue) {
     this.minutes = minutesValue;
     this.secondes = secondesValue;
     this.displayTime();
   },
-  decreaseTime: function() {
+  decreaseTime: function () {
     if (this.secondes === 0) {
       if (this.minutes === 0) {
         this.completeCycle();
@@ -31,63 +33,63 @@ let time = {
     }
     this.displayTime();
   },
-  completeCycle: function() {
+  completeCycle: function () {
     this.stop();
     if (isWorkSession) {
-      alert('Time for a break!');
+      alert("Time for a break!");
       this.setTime(5, 0);
-      isWorkSession = false; 
+      isWorkSession = false;
     } else {
-      alert('Break is over, back to work!');
-      this.setTime(workDuration, 0); 
-      isWorkSession = true; 
+      alert("Break is over, back to work!");
+      this.setTime(workDuration, 0);
+      isWorkSession = true;
     }
-    this.start(); 
+    this.start();
   },
-  start: function() {
+  start: function () {
     if (this.timer === null) {
       this.timer = setInterval(() => this.decreaseTime(), 1000);
     }
   },
-  stop: function() {
+  stop: function () {
     clearInterval(this.timer);
     this.timer = null;
   },
-  reset: function() {
+  reset: function () {
     this.stop();
-    isWorkSession = true; 
-    this.setTime(workDuration, 0); 
+    isWorkSession = true;
+    this.setTime(workDuration, 0);
     this.displayTime();
-  }
+  },
 };
+let controlButtons = document.querySelector(".control-buttons");
 
-document.getElementById('startButton').addEventListener('click', () => {
-  if (time.timer === null) { 
+let pomodoroButton = createButton("startButton", "Start", () => {
+  if (time.timer === null) {
+    pomodoroButton.textContent = "Stop";
+    pomodoroButton.style.backgroundColor = "#000";
     time.start();
+  } else {
+    pomodoroButton.style.backgroundColor = "#7650e0";
+    pomodoroButton.textContent = "Resume";
+    time.stop();
   }
 });
 
-document.getElementById('stopButton').addEventListener('click', () => {
-  time.stop();
-});
+controlButtons.appendChild(pomodoroButton);
 
-document.getElementById('resetButton').addEventListener('click', () => {
-  time.reset();
-});
-
-document.getElementById('decrementButton').addEventListener('click', () => {
+document.getElementById("decrementButton").addEventListener("click", () => {
   if (time.minutes > 1 || (time.minutes === 1 && time.secondes > 0)) {
-    time.stop(); 
+    time.stop();
     time.setTime(time.minutes - 1, 0);
-    workDuration = time.minutes; 
+    workDuration = time.minutes;
   }
 });
 
-document.getElementById('incrementButton').addEventListener('click', () => {
-  time.stop(); 
+document.getElementById("incrementButton").addEventListener("click", () => {
+  time.stop();
   time.setTime(time.minutes + 1, 0);
-  workDuration = time.minutes; 
+  workDuration = time.minutes;
 });
-
 
 time.displayTime();
