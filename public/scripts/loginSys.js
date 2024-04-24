5334d41594645ec4fb3c4e5a342a0044870f0884
UgoW <ugo.warembourg@student.junia.com>
2024-04-25 00:55:32 +0200

    Clean Code

--- public/scripts/loginSys.js
+++ public/scripts/loginSys.js
@@ -13,18 +13,3 @@ fetch('/api/session')
     .catch(error => {
       console.error('Error checking session:', error);
     });
-
-
-document.getElementById("logout-button").addEventListener("click", function () {
-    fetch("http://localhost:3000/api/logout", {
-        method: "GET",
-        credentials: "include",
-    })
-        .then((response) => response.json())
-        .then(data => {
-        window.location.href = "http://localhost:3000/login.html";
-        })
-        .catch((error) => {
-        console.error("Error logging out:", error);
-        });
-    });
---
+++ public/scripts/logout.js
@@ -0,0 +1,13 @@
+document.getElementById("logout-button").addEventListener("click", function () {
+    fetch("http://localhost:3000/api/logout", {
+        method: "GET",
+        credentials: "include",
+    })
+        .then((response) => response.json())
+        .then(data => {
+        window.location.href = "http://localhost:3000/login.html";
+        })
+        .catch((error) => {
+        console.error("Error logging out:", error);
+        });
+    });
