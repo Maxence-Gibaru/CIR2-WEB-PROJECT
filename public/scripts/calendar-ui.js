aa40870407bee6326f9fe3eae4b51ebb11010051
UgoW <ugo.warembourg@student.junia.com>
2024-04-26 11:36:53 +0200

    Clean Calendar UI

--- public/scripts/calendar-ui.js
+++ public/scripts/calendar-ui.js
@@ -1,26 +1,31 @@
-
 export let calendar;
 
-document.addEventListener('DOMContentLoaded', function () {
+// Initialize calendar when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the calendar element
   var calendarEl = document.getElementById('calendar');
+
+  // Create a new FullCalendar instance
   calendar = new FullCalendar.Calendar(calendarEl, {
+    // Set the initial view to timeGridWeek
     initialView: 'timeGridWeek',
-    height: 'auto', // Ajuste automatiquement la hauteur pour éviter le débordement
+    // Automatically adjust height to avoid overflow
+    height: 'auto',
+    // Define the header toolbar configuration
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay listMonth'
     },
+    // Define the events for the calendar
     events: [
       {
         title: 'All Day Event',
         start: '2024-03-01'
-      },
-
+      }
     ]
   });
+
+  // Render the calendar
   calendar.render();
-
-
-
 });
